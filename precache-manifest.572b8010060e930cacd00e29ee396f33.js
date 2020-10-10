0abc58ddce6f9db4bae2263f18af28acce633809
Bhawna Patnaik <b418019@iiit-bh.ac.in>
2020-10-11 01:56:38 +0530

    Updates

---
+++ precache-manifest.572b8010060e930cacd00e29ee396f33.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b10fd3e2012b084ea2c4ed103d144c",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "61997129f63b41c2685c",
+    "url": "/my-website/static/css/2.a0148fe5.chunk.css"
+  },
+  {
+    "revision": "52e2a4692cb4369a8b5d",
+    "url": "/my-website/static/css/main.b8fb50b7.chunk.css"
+  },
+  {
+    "revision": "61997129f63b41c2685c",
+    "url": "/my-website/static/js/2.53f18b9b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-website/static/js/2.53f18b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e2a4692cb4369a8b5d",
+    "url": "/my-website/static/js/main.9fd94a50.chunk.js"
+  },
+  {
+    "revision": "f4f6b37c7c22e9a8d1ee",
+    "url": "/my-website/static/js/runtime-main.063d9c85.js"
+  },
+  {
+    "revision": "c06b81826ca232591b799a623d723381",
+    "url": "/my-website/static/media/colorGame.c06b8182.gif"
+  },
+  {
+    "revision": "fabb0720b83985bc1d5852eccf059323",
+    "url": "/my-website/static/media/logo.fabb0720.svg"
+  },
+  {
+    "revision": "1d79e24972a4986fec7e9adcf37688a3",
+    "url": "/my-website/static/media/resume.1d79e249.jpg"
+  },
+  {
+    "revision": "a5ff9486d24eebdb8a6a67b82b2aefaf",
+    "url": "/my-website/static/media/todoReact.a5ff9486.gif"
+  },
+  {
+    "revision": "18d321375c7fb5cfe4767e73dd41170e",
+    "url": "/my-website/static/media/tutorial.18d32137.gif"
+  },
+  {
+    "revision": "e1b3aff35136780056a28024b35de7dd",
+    "url": "/my-website/static/media/wordcount.e1b3aff3.gif"
+  },
+  {
+    "revision": "6367efe023331bcbc691c340a45d837c",
+    "url": "/my-website/static/media/yelpCamp.6367efe0.gif"
+  }
+]);
